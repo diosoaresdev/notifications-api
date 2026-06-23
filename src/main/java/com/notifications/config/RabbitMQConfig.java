d426c41d1079d1038b6027761bac5a04b14054ab
diosoaresdev <diosoaresdev@gmail.com>
2026-06-23 16:39:04 -0300

    feat: adiciona configuracao do RabbitMQ

---
+++ src/main/java/com/notifications/config/RabbitMQConfig.java
@@ -0,0 +1,51 @@
+package com.notifications.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NOTIFICACOES = "notificacoes.queue";
+    public static final String EXCHANGE_NOTIFICACOES = "notificacoes.exchange";
+    public static final String ROUTING_KEY_NOTIFICACOES = "notificacoes.routingkey";
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.durable(QUEUE_NOTIFICACOES).build();
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NOTIFICACOES);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY_NOTIFICACOES);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+}
